9151cb073e6d573c3c55309a231b575053b35b3f
HOLLOWYVES <cauayves5@gmail.com>
2022-08-21 23:49:21 -0300

    exercicios

---
+++ M-B/aula5-variaveis.js
@@ -0,0 +1,12 @@
+var n = 200;
+var idade = 18
+var nome = "Yves"
+var endereco = "Rio de Janeiro"
+
+//para comentar use duas barras, ou /* */ 
+
+/*variaveis tem varios tipos, sendo eles number, string e boolean os que vao ser estudados.
+saida do terminal: .exit e exit . 
+ctrl + L limpa o terminal */
+
+ 
