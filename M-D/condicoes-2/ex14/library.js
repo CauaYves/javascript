5ef79352941055b7027c8828ffcb2f54d6de4473
HOLLOWYVES <cauayves5@gmail.com>
2022-10-07 18:08:28 -0300

    ex-14

---
+++ M-D/condicoes-2/ex14/library.js
@@ -0,0 +1,9 @@
+function analisar (){
+    var dateObj = new Date();
+    var month = dateObj.getUTCMonth() + 1; //months from 1-12
+    var day = dateObj.getUTCDate();
+    var year = dateObj.getUTCFullYear();
+    
+    newdate = year + "/" + month + "/" + day;
+    console.log(newdate)
+}
