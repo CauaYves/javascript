3e907031b73c311952f7bbaa9040102992f32ceb
HOLLOWYVES <cauayves5@gmail.com>
2022-10-08 10:34:50 -0300

    ex-14

--- M-D/condicoes-2/ex14/library.js
+++ M-D/condicoes-2/ex14/library.js
@@ -1,9 +1,21 @@
+
 function analisar (){
-    var dateObj = new Date();
-    var month = dateObj.getUTCMonth() + 1; //months from 1-12
-    var day = dateObj.getUTCDate();
-    var year = dateObj.getUTCFullYear();
+    let p = document.createElement(`p`)
+    p.setAttribute(`id`, `p`)
+    document.getElementById(`p`)
     
-    newdate = year + "/" + month + "/" + day;
-    console.log(newdate)
+
+    let dateObj = new Date()
+
+    let month = dateObj.getUTCMonth() + 1 //months from 1-12
+    let day = dateObj.getUTCDate()
+    let year = dateObj.getUTCFullYear()
+    let minutes = dateObj.getMinutes()
+    let seconds = dateObj.getSeconds()
+    let miliseconds = dateObj.getMilliseconds()
+    
+
+    p.innerHTML = `${miliseconds} milisegundos<br> ${seconds} segundos <br> ${minutes} minutos <br> ${year} ano atual <br> ${month} mês atual<br> ${day} dia de hoje`
+
+    document.body.appendChild(p)
 }
