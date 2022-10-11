39d851edd1df32b5638928372f237237a3024842
HOLLOWYVES <cauayves5@gmail.com>
2022-10-11 10:37:49 -0300

    ex-16

--- M-D/condicoes-2/ex16/library.js
+++ M-D/condicoes-2/ex16/library.js
@@ -1,4 +1,19 @@
 function gerar() {
-    var num = Math.random(1, 100)
-    console.log(num)
+    let min = 1
+    let max = 100
+
+    let dif = min - max
+    let aleatorio = Math.random()
+    
+    let num = ((min*2) + Math.trunc(aleatorio * dif)) * -1
+
+    var p = document.createElement('p')
+    p.innerHTML = `numero gerado foi ${num}`
+    document.body.appendChild(p)
+
+}
+
+function limpar(){
+    window.location.reload()
+    document.body.append()
 }
