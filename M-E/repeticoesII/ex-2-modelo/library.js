6ffbdebd6f32dc70f9a9aa7e126a6d029af4b988
HOLLOWYVES <cauayves5@gmail.com>
2022-10-18 12:11:04 -0300

    repeticoes

---
+++ M-E/repeticoesII/ex-2-modelo/library.js
@@ -0,0 +1,11 @@
+function contador() { 
+    let primeiroNum = Number(document.getElementById('inumprimeiro').value)
+    let segundoNum = Number(document.getElementById('inumsegundo').value)
+    let res = document.getElementById('res')
+    let passo = Number(document.getElementById('ipasso').value)
+        while(primeiroNum <= segundoNum){
+            res.innerText += `${primeiroNum},`
+            primeiroNum++
+    }
+
+} 
