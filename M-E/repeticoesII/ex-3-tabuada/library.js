9fbaae05d06734e304761024fceb293860974cc5
HOLLOWYVES <cauayves5@gmail.com>
2022-10-21 15:57:01 -0300

    repeticoes-tabuada

--- M-E/repeticoesII/ex-3-tabuada/library.js
+++ M-E/repeticoesII/ex-3-tabuada/library.js
@@ -1,12 +1,16 @@
 let numero = document.getElementById('numero')
 let p = document.getElementById('p')
-function tabuada() {
-    for(i = 1 ; i < 11; i++){
-        p.innerText = `${i * numero}`
-        console.log(i)
-    }
-}
 
+function tabuada(){ 
+
+    for(var i = 0; i < 11; i++){
+        p = `1 x ${i} = ${i * numero} <br>`
+        console.log (p)
+        console.log(numero)
+    }
+
+} 
 function limpar() {
+    document.getElementById('p').innerHTML=''
    document.getElementById('numero').value=''
 }
