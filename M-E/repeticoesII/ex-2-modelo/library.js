a917b2969ea3320148e8a2913365802c1cffb701
HOLLOWYVES <cauayves5@gmail.com>
2022-10-21 13:53:39 -0300

    correcao de logica

--- M-E/repeticoesII/ex-2-modelo/library.js
+++ M-E/repeticoesII/ex-2-modelo/library.js
@@ -2,17 +2,26 @@ function contador() {
     let primeiroNum = Number(document.getElementById('inumprimeiro').value)
     let segundoNum = Number(document.getElementById('inumsegundo').value)
     let p = document.getElementById('p')
-    let passo = Number(document.getElementById('ipasso').value)
-        while(primeiroNum <= segundoNum){
+    let passo = Number(document.getElementById('ipasso').value)   
 
-            if(passo == 0){
-                passo = 1
-            }
-
-            p.innerText += `${primeiroNum},`
-            primeiroNum = primeiroNum + passo
+    if(primeiroNum == 0 || segundoNum == 0 || passo == 0){
+        alert('faltam dados')
+    }else{
+        while(primeiroNum < segundoNum){
+        primeiroNum = primeiroNum + passo
+        p.innerText += `${primeiroNum } >`
+        }
     }
-} 
+    if (segundoNum < primeiroNum){
+        while (primeiroNum > segundoNum) {
+            primeiroNum = primeiroNum - passo
+            p.innerText += `${primeiroNum} >`
+        }
+    }
+}
+
+
+
 function limpar() {
    window.location.reload(true)
 }
