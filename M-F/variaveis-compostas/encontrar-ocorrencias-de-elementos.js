5ce2053bd85a95925a6e3c3740f24bb23513dbd8
HOLLOWYVES <cauayves5@gmail.com>
2022-10-29 23:44:00 -0300

    funcao-trocar-variavel

--- M-F/variaveis-compostas/encontrar-ocorrencias-de-elementos.js
+++ M-F/variaveis-compostas/encontrar-ocorrencias-de-elementos.js
@@ -1,10 +1,35 @@
-var indices = [];
-var array = ['a', 'b', 'a', 'c', 'a', 'd'];
-var elemento = 'a';
-var idx = array.indexOf(elemento);
-while (idx != -1) {
-  indices.push(idx);
-  idx = array.indexOf(elemento, idx + 1);
-}
-console.log(indices);
-// [0, 2, 4]
+function contaLetras(string, letra) {
+    let quantidade = 0;
+    
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] === letra) {
+        quantidade++;
+      }
+    }
+  
+    return quantidade;
+  }
+  contaLetras('carro', 'r')
+  console.log(quantidade)
+
+  function trocaVogais(string) {
+
+    let stringvogal= ''
+  
+    let a = 'a'
+    let e = 'e'
+    let i = 'i'
+    let o = 'o'
+    let u = 'u'
+  
+    for (let i = 0; i < string.length; i++){
+      if (string[i] === a || string[i] === e || string[i] === i || string[i] === o || string[i] === u) {
+        stringvogal = 1
+      }else{
+        stringvogal = string[i]
+      }
+    }
+    console.log(stringvogal)
+    return stringvogal
+
+  }
---
+++ M-F/variaveis-compostas/trocando-vogal.js
@@ -0,0 +1,36 @@
+/*
+function trocaVogais(string) {
+
+    let stringvogal= ''
+  
+    let a = 'a'
+    let e = 'e'
+    let i = 'i'
+    let o = 'o'
+    let u = 'u'
+  
+    for (let i = 0; i < string.length; i++){
+      if (string[i] === a || string[i] === e || string[i] === i || string[i] === o || string[i] === u) {
+        stringvogal = 1
+      }else{
+        stringvogal = string[i]
+      }
+    }
+    console.log(stringvogal)
+    return stringvogal
+
+  }
+  */
+  function tiraNumeros(string) {
+    let numlist = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]
+    for (let i = 0; i < string.length; i++){
+        for(let k = 0; k < numlist.length; k++){
+            if(string[i] == numlist[k]){
+                string.replace(string[i], '');
+            }
+        }
+    }
+    console.log(string)
+    return string
+    }
+    tiraNumeros('c1a2u3a4')
