dfac7e356c12b8f24331b3a7514259d46c8fa174
HOLLOWYVES <cauayves5@gmail.com>
2022-11-10 23:05:21 -0300

    Update productFromTheBiggest.js

--- exercicios/productFromTheBiggest.js
+++ exercicios/productFromTheBiggest.js
@@ -2,15 +2,15 @@ function solution(inputArray) {
     let maiorUm = 0
     let maiorDois = 0
     for(let i = 0; i < inputArray.length; i++){
-        if(inputArray[i] > maiorUm){
+        if(inputArray[i] > maiorUm && inputArray[i] % 3 == 0){
             maiorUm = inputArray[i]
         }
     }
     for (let j = 0; j < inputArray.length; j++){
-        if(inputArray[j] > maiorDois && inputArray[j] != maiorUm){
+        if(inputArray[j] > maiorDois && inputArray[j] != maiorUm && inputArray[j] % 3 == 0){
             maiorDois = inputArray[j]
         }
     }
 return maiorDois * maiorUm
 } 
-console.log(solution( [3, 6, -2, -5, 7, 3] )) // 2
+console.log(solution( [3, 6, -2, -5, 7, 3] )) // 21
