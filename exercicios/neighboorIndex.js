de0ce67e3f5aaf93d952879ca7d974b5acdd8c17
HOLLOWYVES <cauayves5@gmail.com>
2022-11-12 15:02:57 -0300

    Update neighboorIndex.js

--- exercicios/neighboorIndex.js
+++ exercicios/neighboorIndex.js
@@ -1,12 +1,12 @@
 function solution(inputArray) {
-    let nUm = 0
-
+    let nUm = inputArray[0] * inputArray[1]
     for(let i = 0; i < inputArray.length; i++){
         if(inputArray[i] * inputArray[i + 1] > nUm){
             nUm = inputArray[i] * inputArray[i + 1]
-        }
+        }   
     }
     return nUm
 }
 
-console.log(solution([3, 6, -2, -5, 7, 3]))
+
+console.log(solution([-23, 4, -3, 8, -12])) // -12
